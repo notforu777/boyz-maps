01c434a74901e6851e00df5a3e946150f32c0eda
Nikita Bogatyrev <nik.bogatyrev.02@mail.ru>
2019-06-16 16:34:09 +0300

    добавил активити маркера ещё без картинки но с описанием и названиеи

--- app/src/main/java/com/jbsummer2019/bmnboyzmapapp/MarkerActivity.kt
+++ app/src/main/java/com/jbsummer2019/bmnboyzmapapp/MarkerActivity.kt
@@ -2,13 +2,15 @@ package com.jbsummer2019.bmnboyzmapapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_marker.*
 
 class MarkerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chizh_pyzh)
+        setContentView(R.layout.activity_marker)
 
-        intent.getStringExtra("id")
+        name.text=intent.getStringExtra("title")
+        text.text=intent.getStringExtra("text")
     }
 }
