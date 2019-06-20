c6cb6855f371aa18e474c321e7bb4471a3fffe35
xenia marchenko <ksumar4enko@gmail.com>
2019-06-20 22:48:01 +0300

    дизайн менюшки для стилей

--- app/src/main/java/com/jbsummer2019/bmnboyzmapapp/Style_map_activity.kt
+++ app/src/main/java/com/jbsummer2019/bmnboyzmapapp/Style_map_activity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.view.Menu
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.activity_style_map_activity.*
-import kotlinx.android.synthetic.main.activity_style_map_activity.button_back_from_menu
+import kotlinx.android.synthetic.main.activity_style_map_activity.button_back_from_styles_menu
 
 class Style_map_activity : AppCompatActivity() {
 
@@ -14,7 +14,7 @@ class Style_map_activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_style_map_activity)
 
-        button_style_1.setOnClickListener {
+        button_style_4.setOnClickListener {
             val intent= Intent(this,MapsActivity::class.java)
             intent.putExtra("Style","dark")
             startActivity(intent)
@@ -32,25 +32,25 @@ class Style_map_activity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        button_style_4.setOnClickListener {
+        button_style_5.setOnClickListener {
             val intent= Intent(this,MapsActivity::class.java)
             intent.putExtra("Style","night")
             startActivity(intent)
         }
 
-        button_style_5.setOnClickListener {
+        button_style_6.setOnClickListener {
             val intent= Intent(this,MapsActivity::class.java)
             intent.putExtra("Style","aubergine")
             startActivity(intent)
         }
 
-        button_style_6.setOnClickListener {
+        button_style_1.setOnClickListener {
             val intent= Intent(this,MapsActivity::class.java)
             intent.putExtra("Style","standart")
             startActivity(intent)
         }
 
-        button_back_from_menu.setOnClickListener {
+        button_back_from_styles_menu.setOnClickListener {
             val intent= Intent(this,Menu::class.java)
             startActivity(intent)
         }
